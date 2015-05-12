dec00327b30f5f6c78f3d31f4d46f72dc3456031
wqchen68 <wqchen68@gmail.com>
2015-05-12 17:22:38 +0800

    test

---
+++ GIS/gis_china.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+library(gpclib)
+library(maptools)
+# 讀取地理資訊數據
+# load(url("http://gadm.org/data/rda/CHN_adm1.RData"))
+load(paste0("/home/wqchen/R/Rscript/CHN_adm1.RData"))
+# 人均水資源量
+water <- c(1085,325,1473,3524,1079,2935,3989,2790,4147,358,2046,434
+           ,1652,2490,451,3362,1467,871,2145,182,1000,12278,448,377,
+           182,1221,3135,152,4976,10000,5298,2005)
+# 將數據轉為data.frame
+gpclibPermit()
+china.map <- fortify(gadm,region='ID_1')
+vals <- data.frame(id =unique(china.map$id),val=water)
+# 用ggplot2命令繪圖
+ggplot(vals, aes(map_id = id)) + 
+  geom_map(aes(fill = val), map =china.map) +
+  expand_limits(x = china.map$long, y = china.map$lat) +
+  scale_fill_continuous(low = 'red2',high ='yellowgreen',
+                        guide = "colorbar") + 
+  theme(axis.text=element_blank(),
+        axis.ticks=element_blank(),
+        axis.title=element_blank(),
+        panel.grid.major=element_blank(),
+        panel.background=element_blank()) +
+  xlab("") + ylab("")
